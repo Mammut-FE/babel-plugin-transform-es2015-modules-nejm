35e561c2a1f324d5302983abc26cb9bc82ef7325
lleohao <lleohao@hotmail.com>
2017-11-10 22:36:54 +0800

    :tada: 完成

--- src/index.ts
+++ src/index.ts
@@ -1,126 +1,57 @@
-import template from "@babel/template";
-import {
-    isModule,
-    rewriteModuleStatementsAndPrepareHeader,
-    isSideEffectImport,
-    buildNamespaceInitStatements,
-    ensureStatementsHoisted,
-    wrapInterop,
-} from "babel-helper-module-transforms";
+import template from '@babel/template';
+
+import { isModule, helper, NodePath } from './helper';
+import { Program, ExportDefaultDeclaration, returnStatement } from 'babel-types';
 
 const buildWrapper = template(`
   define(AMD_ARGUMENTS, function(IMPORT_NAMES) {
   })
 `);
 
-function transformNEJDefine({ nejmUrl = './nejm.js', packages = [] }) {
-    const nejModule = packages.map(packageName => {
-        return new RegExp('\^(' + packageName + ')\/');
-    });
-
-    const textModule = /\.json$|\.html$|\.css$/;
-    const nejm = /^nejm$/;
-
-    return function (importName) {
-        if (textModule.test(importName)) {
-            importName = 'text!' + importName;
-        } else {
-            if (nejm.test(importName)) {
-                importName = nejmUrl;
-            }
-
-            nejModule.forEach(m => {
-                if (m.test(importName)) {
-                    importName = importName.replace(m, '{$1}');
-                }
-            });
-
-            if (!importName.endsWith('.js')) {
-                importName += '.js';
-            }
-        }
-
-
-        return importName;
-    }
-}
-
 export default function ({ types: t }) {
     return {
         visitor: {
             Program: {
                 exit(path, { opts }) {
-                    const _ = transformNEJDefine(opts);
                     if (!isModule(path)) return;
 
-                    const {
-                        meta,
-                        headers,
-                    } = rewriteModuleStatementsAndPrepareHeader(path, {
-                            loose: false,
-                            strict: false,
-                            strictMode: false,
-                            allowTopLevelThis: false,
-                            noInterop: false
-                        });
+                    const { nejDefines, fnBody, exportDeclaration } = helper(path, opts);
 
                     const amdArgs = [];
                     const importNames = [];
 
-                    for (const [source, metadata] of meta.source) {
-                        amdArgs.push(t.stringLiteral(_(source)));
-                        importNames.push(t.identifier(metadata.name));
+                    nejDefines.forEach(({ source, name }) => {
+                        amdArgs.push(t.stringLiteral(source));
+                        name && importNames.push(t.identifier(name));
+                    });
 
-                        if (!isSideEffectImport(metadata)) {
-                            const interop = wrapInterop(
-                                path,
-                                t.identifier(metadata.name),
-                                metadata.interop,
-                            );
-                            if (interop) {
-                                const header = t.expressionStatement(
-                                    t.assignmentExpression(
-                                        "=",
-                                        t.identifier(metadata.name),
-                                        interop,
-                                    ),
-                                );
-                                header.loc = metadata.loc;
-                                headers.push(header);
-                            }
-                        }
-
-                        headers.push(...buildNamespaceInitStatements(meta, metadata));
+                    if (exportDeclaration) {
+                        fnBody.push(t.returnStatement((exportDeclaration as ExportDefaultDeclaration).declaration));
                     }
 
-                    ensureStatementsHoisted(headers);
-                    path.unshiftContainer("body", headers);
+                    const { body, directives } = <Program>path.node;
 
-                    const { body, directives } = path.node;
+                    (path.node as Program).directives = [];
+                    (path.node as Program).body = [];
 
-                    path.node.directives = [];
-                    path.node.body = [];
-
-                    path.pushContainer("body", [
-                        buildWrapper({
-                            AMD_ARGUMENTS: t.arrayExpression(amdArgs),
-                            IMPORT_NAMES: importNames,
-                        }),
-                    ]);
+                    path.pushContainer('body', [buildWrapper({
+                        AMD_ARGUMENTS: t.arrayExpression(amdArgs),
+                        IMPORT_NAMES: importNames
+                    })]);
 
                     // 处理 return 数据
-                    const amdWrapper = path.get("body")[0];
+                    const amdWrapper = path.get('body')[0];
                     const amdFactory = amdWrapper
-                        .get("expression.arguments")
+                        .get('expression.arguments')
                         .filter(arg => arg.isFunctionExpression())[0]
-                        .get("body");
+                        .get('body');
 
-                    amdFactory.pushContainer("directives", t.VariableDeclaration('const', [t.VariableDeclarator(t.identifier('_export'), t.ObjectExpression([]))]));
-                    amdFactory.pushContainer("directives", directives);
-                    amdFactory.pushContainer("body", body);
-                    amdFactory.pushContainer("body", t.returnStatement(t.identifier('_export')));
-                },
-            },
-        },
+                    amdFactory.pushContainer('directives', directives);
+                    amdFactory.pushContainer('body', fnBody);
+                }
+            }
+        }
     };
 }
+
+
